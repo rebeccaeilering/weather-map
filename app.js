b233ea04a17acfa3a4440fea137c6a026a9b710b
Rebecca Eilering <rebeccaeilering@gmail.com>
2020-07-02 22:06:04 -0500

    added a views folder and path for homepage

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ app.listen(3000, () => {
   console.log(`Starting server`);
 });
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', { root: __dirname});
+})
